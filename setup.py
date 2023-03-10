01db53cbd75aefab17aef2cc8e243e5d8f272d45
prvn386 <prvn386@gmail.com>
2023-03-10 23:03:35 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import  find_packages,setup
+from typing import List 
+
+
+Hypen_e_dot = '-e .'
+def get_requirements(file_path:str) -> List[str]:
+    '''
+        This function will return the list of requirements
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n','') for req in requirements]
+
+        if Hypen_e_dot in requirements:
+            requirements.remove(Hypen_e_dot)
+
+    return requirements
+
+setup(
+name = 'mlproject',
+version = '0.0.1',
+author = 'praveen',
+author_email = 'prvn386@gmail.com',
+packages = find_packages(),
+install_requires = get_requirements('requirements.txt')
+
+)
